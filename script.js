3170509b009ec43b588ba1de2f7bb8562d78959e
Prachi Gore <prachigore408@gmail.com>
2024-03-05 19:11:13 +0530

    canvas width height set

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+   var c= document.querySelector('canvas');
+   var canvas= document.querySelector('canvas');
+   c.width=innerWidth;
+   c.height=innerHeight;
+   c.getContext('2d')
+   console.log('c',c.getContext('2d'))
+   // start
+}
